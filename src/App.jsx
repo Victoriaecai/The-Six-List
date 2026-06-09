ccb4904126ec17160d4732e80a9fbdc5da851d7f
Victoria <caiduo813006@gmail.com>
2026-06-09 14:39:11 -0400

    fixed loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import { useState, useEffect } from 'react'
 import EventCard from './components/EventCard'
+import { fetchTorontoEvents } from './services/ticketmaster'
 
 
 function App() {
   const [events, setEvents] = useState([])
 
   useEffect(() => {
-    fetchTorontoEvents().then(events => setEvents(events))
+  fetchTorontoEvents().then(events => {
+    console.log(events)
+    setEvents(events)
+    })
   }, [])
 
   return (
     <div>
       <h1>The Six List</h1>
       {events.map(event => (
-        <EventCard 
-          key={event.id}
-          name={event.name} 
-          date={event.dates.start.localDate}
-          location={event._embedded.venues[0].name}
-          image={event.images[0].url}
-          category={event.classifications[0].segment.name}
-        />
-      ))}
+          <EventCard
+            key={event.id}
+            name={event.name}
+            date={event.dates?.start?.localDate}
+            location={event._embedded?.venues?.[0]?.name}
+            image={event.images?.[0]?.url}
+            category={event.classifications?.[0]?.segment?.name}
+          />
+        ))}
     </div>
   )
 }
+
 
 export default App
